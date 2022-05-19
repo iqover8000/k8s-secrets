749ec3ac2784c84cc8c84b724d4683848a6ac124
Fedor Shevchuk <iqover8000@gmail.com>
2022-05-19 16:36:34 +0000

    add exception handlong

--- k8s-secrets.py
+++ k8s-secrets.py
@@ -9,37 +9,46 @@ from base64 import b64decode
 
 
 def upload_to_pastebin(paste):
-    headers = {
-        'Content-Type': 'application/x-www-form',
-    }
-    params = {
-        "burn": "true",
-        "ttl": 300
-    }
-    response = post(
-        url=PASTE_URL,
-        data=paste,
-        params=params,
-        headers=headers
-    )
-    return response.text
+    try:
+        headers = {
+            'Content-Type': 'application/x-www-form',
+        }
+        params = {
+            "burn": "true",
+            "ttl": 300
+        }
+        response = post(
+            url=PASTE_URL,
+            data=paste,
+            params=params,
+            headers=headers
+        )
+        return response.text
+    except Exception as e:
+        print(f"Got exception while uploading paste. Exception: {e}")
 
 
 def get_secrets():
-    secrets = v1.list_secret_for_all_namespaces()
-    for i in secrets.items:
-        print(f"==> {colored(i.metadata.name, 'yellow')} (namespace: {i.metadata.namespace}):")
-        for k, v in i.data.items():
-            print(f"      {colored(k, 'green')}: ***hidden*secret***")
-        print()
+    try:
+        secrets = v1.list_secret_for_all_namespaces()
+        for i in secrets.items:
+            print(f"==> {colored(i.metadata.name, 'yellow')} (namespace: {i.metadata.namespace}):")
+            for k, v in i.data.items():
+                print(f"      {colored(k, 'green')}: ***hidden*secret***")
+            print()
+    except Exception as e:
+        print(f"Got exception while getting all secrets from cluster. Exception: {e}")
 
 
 def get_named_secret(name, namespace):
-    secret = v1.read_namespaced_secret(name, namespace)
-    print(f"==> {colored(secret.metadata.name, 'yellow')} (namespace: {secret.metadata.namespace}):")
-    for k, v in secret.data.items():
-        paste_link = upload_to_pastebin(b64decode(v))
-        print(f"      {colored(k, 'green')}: {paste_link}")
+    try:
+        secret = v1.read_namespaced_secret(name, namespace)
+        print(f"==> {colored(secret.metadata.name, 'yellow')} (namespace: {secret.metadata.namespace}):")
+        for k, v in secret.data.items():
+            paste_link = upload_to_pastebin(b64decode(v))
+            print(f"      {colored(k, 'green')}: {paste_link}")
+    except Exception as e:
+        print(f"Got exception while getting secret from cluster. Exception: {e}")
 
 
 if __name__ == "__main__":
